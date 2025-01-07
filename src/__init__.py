8414eb6dfc3252275d51e80d71a2b8cc2b692463
goldirana <goldirana3210@gmail.com>
2025-01-07 18:20:22 +0530

    added read_yaml

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,7 @@
 import logging
 import os
+import yaml
+
 
 log_dir = "logs"
 log_file_name = "running.log"
@@ -18,3 +20,16 @@ logging.basicConfig(
     handlers=[file_handler, stream_handler]
 )
 logger = logging.getLogger("Initializing logging")
+
+
+def read_yaml(path, format="r"):
+    try:
+        with open(path, "r") as f:
+            params = yaml.safe_load(f)
+            logging.info("Yaml read successfully from %s", path)
+            return params
+    except FileNotFoundError:
+        logger.error("FileNotFoundError: %s", path)
+    except Exception as e:
+        logger.error(f"Exception occured while reading yaml file from \
+                        location: {path}\n {e}")
