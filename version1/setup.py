0da345ec68db5c345cb7b15ddd7dbd969e1f5a08
goldirana <goldirana3210@gmail.com>
2025-01-07 16:12:05 +0530

    setting up the structure

---
+++ version1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Telco customer churn is a dataset hosted by IBM. This project is used to implement end to end project using concepts like modular programming and tools like mlflow, dvc, and git.',
+    author='Rajesh Goldy',
+    license='MIT',
+)
