ee2de8b269031a959705bdc6ba2e77d455830474
goldirana <goldirana3210@gmail.com>
2025-01-08 08:39:54 +0530

    added directory functions

--- src/utils/common.py
+++ src/utils/common.py
@@ -1,12 +1,53 @@
 import json, yaml
 from src import logger
+from box import ConfigBox
+from ensure import ensure_annotations
+import os
+from pathlib import Path
 
-def read_yaml(path, format="r"):
+
+def create_directory(path: str, is_extension_present: bool=True)-> None:
+    if is_extension_present: # remove extension to create directory
+            path = str(split_file_extension(path))
+    _ = check_directory_path(path)
+    try:
+        if _ == False:
+            os.makedirs(path, exist_ok=True) # create directoy at given location
+            logger.info("Creating directory at %s", path)
+        else: 
+            os.makedirs(str(path), exist_ok=True)
+            logger.info("Deleting previous directory and creating directory at %s", path)
+    except Exception as e:
+        logger.error(f"Error occured while creating directory at {path} \n{e}")
+
+def split_file_extension(path: str) -> str:
+    try:
+        root_dir = str(Path(path).resolve().parent)
+        logger.debug("File path without extension is %s", root_dir)
+        return root_dir
+    except Exception as e:
+        logger.error(e)
+
+
+def check_directory_path(path: str) -> None:
+    try:
+        check = os.path.isdir(path)
+        if check:
+            logger.info("Directory already present at %s", path)
+            return True
+        else:
+            logger.info("Directory not present at %s", path)
+            return False
+    except Exception as e:
+        logger.error(e)
+
+@ensure_annotations
+def read_yaml(path: str, format: str="r"):
     try:
         with open(path, "r") as f:
             params = yaml.safe_load(f)
             logger.info("Yaml read successfully from %s", path)
-            return params
+            return ConfigBox(params)
     except FileNotFoundError:
         logger.error("FileNotFoundError: %s", path)
     except Exception as e:
